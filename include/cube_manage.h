5f62a6bb774698b8fd4894d32c86c3218e5d6e46
Hu jun <algorist@bjut.edu.cn>
2018-06-28 23:31:05 -0700

    cube manage first version

--- include/cube_manage.h
+++ include/cube_manage.h
@@ -3,16 +3,90 @@
 
 enum dtype_cube_manage
 {
-	DTYPE_CUBE_MANAGE=0x310,
+	TYPE(CUBE_MANAGE_BASIC)=0x310,
+	TYPE(CUBE_MANAGE_DATA),
+	TYPE(CUBE_MANAGE_MODULE),
+	TYPE(CUBE_MANAGE_ENTITY),
+	TYPE(CUBE_MANAGE_ROUTE),
+	TYPE(CUBE_MANAGE_CONN),
+	TYPE(CUBE_MANAGE_CHANNEL)
 };
-enum subtype_cube_manage
+enum subtype_cube_manage_basic
 {
-	SUBTYPE_CUBE_NODE_INFO=0x01,
-	SUBTYPE_CUBE_INSTANCE_INFO,
-	SUBTYPE_CUBE_CONF_REQ,
-	SUBTYPE_CUBE_CONF_FILE,
+	SUBTYPE(CUBE_MANAGE_BASIC,HEAD)=0x01,
+	SUBTYPE(CUBE_MANAGE_BASIC,REPLY)
+};
+enum subtype_cube_manage_data
+{
+	SUBTYPE(CUBE_MANAGE_DATA,FORMAT)=0x01,
+	SUBTYPE(CUBE_MANAGE_DATA,ELEM),
+	SUBTYPE(CUBE_MANAGE_DATA,TYPE_GROUP),
+	SUBTYPE(CUBE_MANAGE_DATA,TYPE),
+	SUBTYPE(CUBE_MANAGE_DATA,SUBTYPE),
+	SUBTYPE(CUBE_MANAGE_DATA,RECORDTYPE),
+	SUBTYPE(CUBE_MANAGE_DATA,RECORD)
+};
+enum subtype_cube_manage_entity
+{
+	SUBTYPE(CUBE_MANAGE_ENTITY,NODE_INFO)=0x01,
+	SUBTYPE(CUBE_MANAGE_ENTITY,INSTANCE_INFO)
+};
+enum enum_manage_action
+{
+	GET=0x01,
+	POST,
+	HEAD,
+	DELETE,
+	REPLY=0x10	
 };
 
+enum enum_manage_class
+{
+	CUBE_MANAGE_DATA=0x01,
+	CUBE_MANAGE_ENTITY
+};
+
+enum flag_cube_elem_attr
+{
+        FLAG_ELEM_ATTR_POINTER=0x01,
+        FLAG_ELEM_ATTR_DEFINE=0x02,
+        FLAG_ELEM_ATTR_ARRAY=0x04,
+        FLAG_ELEM_ATTR_ENUM=0x10,
+        FLAG_ELEM_ATTR_FLAG=0x20,
+        FLAG_ELEM_ATTR_SUBSET=0x40,
+        FLAG_ELEM_ATTR_VALUE=0x80,
+        FLAG_ELEM_ATTR_NUM=0x100,
+        FLAG_ELEM_ATTR_BOOL=0x200,
+        FLAG_ELEM_ATTR_NAMELIST=0x400,
+        FLAG_ELEM_ATTR_REF=0x800,
+        FLAG_ELEM_ATTR_FIXSIZE=0x1000,
+        FLAG_ELEM_ATTR_SPECIAL=0x2000,
+        FLAG_ELEM_ATTR_EMPTY=0x80000,
+
+};
+
+typedef struct cube_manage_head
+{
+	enum enum_manage_action manage_action; 	
+	enum enum_manage_class manage_class; 	
+	BYTE serialID[32];
+}__attribute__((packed)) RECORD(CUBE_MANAGE_BASIC,HEAD);
+
+typedef struct cube_manage_reply
+{
+	enum enum_manage_action manage_action; 	
+	enum enum_manage_class manage_class; 	
+	int  return_code;
+	BYTE serialID[32];
+}__attribute__((packed)) RECORD(CUBE_MANAGE_BASIC,REPLY);
+
+typedef struct cube_data_format
+{
+	char name[DIGEST_SIZE];
+	int  size;
+	int  attr;  // flag with flag_cube_elem_attr
+	char * desc;
+}__attribute__((packed)) RECORD(CUBE_MANAGE_DATA,FORMAT);
 struct cube_node_info
 {
 	BYTE uuid[DIGEST_SIZE];
