9a0899851af97f5057f03d7786e6b323a55e65c2
Hu jun <algorist@bjut.edu.cn>
2018-11-16 06:18:16 -0800

    let defint_convert receive (MESSAGE,TYPES) msg

--- module/define_convert/define_convert.c
+++ module/define_convert/define_convert.c
@@ -22,6 +22,7 @@
 #include "convert_elem.h"
 // add para lib_include
 
+int proc_types_message(void *sub_proc,void * recv_msg);
 int define_convert_init(void * sub_proc, void * para)
 {
 	int ret;
@@ -36,28 +37,6 @@ int define_convert_start(void * sub_proc, void * para)
 	int subtype;
 	// add yorself's module exec func here
 
-	char Buf[1024];
-
-	ret=convert_type(TYPE(CUBE_MANAGE_BASIC),TYPE(CUBE_MANAGE_BASIC),"enum_cube_manage",Buf);
-
-	if(ret>0)
-		printf("%s\n",Buf);		
-	else
-		printf("convert failed!\n");
-
-	ret=convert_subtype(TYPE(CUBE_MANAGE_BASIC),NULL,Buf);
-
-	if(ret>0)
-		printf("%s\n",Buf);		
-	else
-		printf("convert failed!\n");
-
-	ret=convert_record(TYPE_PAIR(CUBE_MANAGE_BASIC,HEAD),Buf);
-
-	if(ret>0)
-		printf("%s\n",Buf);		
-	else
-		printf("convert failed!\n");
 
 	while(1)
 	{
@@ -75,6 +54,82 @@ int define_convert_start(void * sub_proc, void * para)
 			message_get_type(recv_msg),message_get_subtype(recv_msg));
 			continue;
 		}
+		if((type==TYPE(MESSAGE))&&(subtype==SUBTYPE(MESSAGE,TYPES)))
+		{
+			ret=proc_types_message(sub_proc,recv_msg);
+		}
+
 	}
 	return 0;
+}
+int proc_types_message(void * sub_proc,void * message)
+{
+	int type;
+	int subtype;
+	int i;
+	int ret;
+	char Buf[1024];
+	printf("begin proc types msg \n");
+
+	type=message_get_type(message);
+	subtype=message_get_subtype(message);
+
+	RECORD(MESSAGE,TYPES) * types_pair;
+	
+	i=0;
+
+	ret=message_get_record(message,&types_pair,i++);
+	if(ret<0)
+		return ret;
+	while(types_pair!=NULL)
+	{
+		
+		ret=convert_type(types_pair->type,types_pair->type,"enum_cube_manage",Buf);
+
+		if(ret>0)
+			printf("%s\n",Buf);		
+		else
+			printf("convert failed!\n");
+		if(types_pair->subtype==0)
+		{
+
+			ret=convert_subtype(types_pair->subtype,NULL,Buf);
+
+			if(ret>0)
+				printf("%s\n",Buf);		
+			else
+				printf("convert failed!\n");
+		}
+
+		if(types_pair->subtype!=0)
+		{
+			ret=convert_record(type,subtype,Buf);
+			if(ret>0)
+				printf("%s\n",Buf);		
+			else
+				printf("convert failed!\n");
+		}
+		else
+		{
+			struct struct_namelist * subtypelist;
+			subtypelist=memdb_get_subtypelist(types_pair->type);
+			if(subtypelist==NULL)
+			{
+				printf("get error subtypelist!\n");
+				return -EINVAL;
+			}	
+			for(i=0;i<subtypelist->elem_no;i++)
+			{
+				ret=convert_record(type,subtypelist->elemlist[i].value,Buf);
+				if(ret>0)
+					printf("%s\n",Buf);		
+			}
+			
+		}
+
+		ret=message_get_record(message,&types_pair,i++);
+		if(ret<0)
+			return ret;
+	}
+	return ret;
 }
